208c3e056c48db4f24ff9838e4b2d4a3b833e7b4
tzekiattok <72696034+tzekiattok@users.noreply.github.com>
2021-03-15 13:19:16 +0800

    Add files via upload

---
+++ ESD-Project/SQL-files/reservation.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 06, 2021 at 08:45 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS `reservation` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `reservation`;
+
+
+
+DROP TABLE IF EXISTS `reservation`;
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `reservationID` int AUTO_INCREMENT NOT NULL ,
+  `restaurantID` varchar(64) NOT NULL,
+  `customerID` varchar(64) DEFAULT NULL,
+  `capacity` int NOT NULL,
+  `date` Date NOT NULL,
+  `time` time not NULL,
+  `duration` float NOT NULL,
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`reservationID`,`restaurantID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+--
+
+INSERT INTO `reservation` (`restaurantID`, `capacity`, `date`, `time`,`duration`, `status`,`customerID`) VALUES
+('EighteenChefz', 4, '2021-04-01', '12:00:00', 2.00, 'open',NULL),
+('EighteenChefz', 4, '2021-04-01', '14:00:00', 2.00, 'open',NULL),
+('EighteenChefz', 4, '2021-04-01', '16:00:00', 2.00, 'open','Bartok'),
+('EighteenChefz', 2, '2021-04-01', '18:00', 2.00, 'open',NULL),
+('EighteenChefz', 4, '2021-04-01', '20:00', 2.00, 'open',NULL),
+('EighteenChefz', 6, '2021-04-02', '12:00', 2.00, 'open',NULL),
+('EighteenChefz', 6, '2021-04-02', '14:00', 2.00, 'open',NULL),
+('EighteenChefz', 6, '2021-04-02', '16:00', 2.00, 'open',NULL),
+('EighteenChefz', 6, '2021-04-02', '18:00', 2.00, 'open',NULL),
+('EighteenChefz', 6, '2021-04-02', '20:00', 2.00, 'open',NULL),
+('Mala123', 4, '2021-04-01', '11:00', 1.00, 'open',NULL),
+('Mala123', 5, '2021-04-02', '12:00', 1.00, 'open',NULL),
+('Mala123', 6, '2021-04-02', '13:00', 1.00, 'open',NULL),
+('Mala123', 6, '2021-04-03', '14:00', 1.00, 'open',NULL),
+('Weizhi', 6, '2021-04-03', '14:00', 3.00, 'open',NULL),
+('Weizhi', 4, '2021-04-03', '14:00', 1.00, 'open',NULL),
+('Weizhi', 4, '2021-04-03', '15:00', 1.00, 'open',NULL),
+('Weizhi', 2, '2021-04-03', '16:00', 1.00, 'open',NULL),
+('XJWangz', 8,'2021-04-04', '13:00', 1.00, 'open',NULL),
+('XJWangz', 12, '2021-04-04', '14:00', 1.00, 'open',NULL),
+('XJWangz', 5, '2021-04-04', '18:00', 1.00, 'open',NULL),
+('XJWangz', 4, '2021-04-04', '18:00', 1.00, 'open',NULL);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ ESD-Project/SQL-files/restaurant.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 06, 2021 at 08:45 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurant`
+--
+
+DROP TABLE IF EXISTS `restaurant`;
+CREATE TABLE IF NOT EXISTS `restaurant` (
+  `restaurantID` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `branch` varchar(8) NOT NULL,
+  `category` varchar(64) NOT NULL,
+  `location` varchar(64) NOT NULL,
+  `description` varchar(10000) NOT NULL,
+  `rating` double NOT NULL,
+  `review` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`restaurantID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`restaurantID`, `name`, `branch`, `category`, `location`, `description`, `rating`, `review`) VALUES
+('EighteenChefz', '18 Chefs', 'Novena S', 'Western Cuisine', 'Novena Square #03-12', '18 Chef is a fine dining restaurant for cheap teenagers', 4.1, ''),
+('Mala123', 'Watashiwa Mala', 'Lot 1', 'Chinese Cuisine', 'Lot 1 #01-11', 'Super hot mala for insecure teens', 3.7, ''),
+('Weizhi', 'Wei Zhi China Wine', 'Yew Tee ', 'Wine', 'Yew Tee #B0-01', 'Wei Zhi`s china wine has orginated back from the bodoh dynstasty....', 4.8, ''),
+('XJWangz', 'Wang`s and Wank`s', 'Tekong', 'Chinese Cuisine', 'Tekong Ferry', 'Eat some tekong boys like xing jie', 1, '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
